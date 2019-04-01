d8f9783326d5cfeef1268db9b845d4f555216d44
Connor Larkin <connor.larkin1@gmail.com>
2019-03-31 21:26:00 -0500

    up and running

--- ios/RNReactNativeImageLabel.m
+++ ios/RNReactNativeImageLabel.m
@@ -2,6 +2,8 @@
 #import "RNReactNativeImageLabel.h"
 #import <React/RCTLog.h>
 
+@import Firebase;
+
 @implementation RNReactNativeImageLabel
 
 RCT_EXPORT_MODULE();
@@ -12,8 +14,32 @@ RCT_EXPORT_MODULE();
 }
 
 
-RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+RCT_EXPORT_METHOD(addEvent:(NSString *)imagePath location:(NSString *)location)
 {
+    UIImage *uiImage = [UIImage imageWithContentsOfFile:imagePath];
+    
+    FIRVisionImage *image = [[FIRVisionImage alloc] initWithImage:uiImage];
+    
+    FIRVisionImageLabeler *labeler = [[FIRVision vision] onDeviceImageLabeler];
+
+    [labeler processImage:image
+               completion:^(NSArray<FIRVisionImageLabel *> *_Nullable labels,
+                            NSError *_Nullable error) {
+                   if (error != nil) { return; }
+                   
+                   
+                   
+                   // Task succeeded.
+                   // ...
+                   
+                   for (FIRVisionImageLabel *label in labels) {
+                       NSString *labelText = label.text;
+                       NSString *entityId = label.entityID;
+                       NSNumber *confidence = label.confidence;
+                        RCTLogInfo(@"ML SHIT %@ at %@", labelText, entityId);
+                   }
+               }];
+
     RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
 
